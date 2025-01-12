00f32a9b652cba4ad928c6543e9c22450abb5c04
VISHWANATH TAWARE <itsvishwa23@gmail.com>
2025-01-12 09:23:15 +0530

    Appium Project Setup

---
+++ SEED_SDET_COURSE_APPIUM_MOBILE_TESTING/src/test/java/SEED_SDET_COURSE_APPIUM_MOBILE_TESTING/SEED_SDET_COURSE_APPIUM_MOBILE_TESTING/AppTest.java
@@ -0,0 +1,38 @@
+package SEED_SDET_COURSE_APPIUM_MOBILE_TESTING.SEED_SDET_COURSE_APPIUM_MOBILE_TESTING;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
